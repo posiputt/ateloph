1db4cfa7e05e67ffe01aeac0b91953ad4f1eb918
Andrej Schoeke <github@andrej-schoeke.de>
2015-01-16 16:25:34 +0100

    Experiments

--- Sandbox/minibot.py
+++ Sandbox/minibot.py
@@ -1,17 +1,18 @@
 import socket
 
 s = socket.socket()
-s.connect(('chat.freenode.net', 6667))
-s.send('NICK atelophn')
-s.send('USER posiputt irc.freenode.net bla: atelophbotn')
+with s.connect(('chat.freenode.net', 6667)):
+    s.send('NICK ateloph_aos\n')
 
-while True:
-    line = s.recv(500)
-    print line
-    if line.find('Welcome to the freenode') != -1:
-        s.send('JOIN #5n')
-    if line.find('PRIVMSG') != -1:
-        line = line.rstrip()
-        line = line.split()
+    s.send('USER ateloph_aos irc.freenode.net bla: atelophbot\n')
+
+    while True:
+        line = s.recv(500)
+        print line
+        if line.find('Welcome to the freenode') != -1:
+            s.send('JOIN #5\n')
+        if line.find('PRIVMSG') != -1:
+            line = line.rstrip()
+            line = line.split()
         if (line[0] == 'PING'):
-            s.send('PONG '+line[1]+'n')
+            s.send('PONG '+line[1]+'\n')
