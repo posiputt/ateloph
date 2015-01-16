93f19f94bec96007e8102cd33757a6c6e13b1659
Andrej Schoeke <github@andrej-schoeke.de>
2015-01-16 20:42:29 +0100

    More functionalized.

--- Sandbox/minibot.py
+++ Sandbox/minibot.py
@@ -1,4 +1,12 @@
 import socket
+import sys
+
+
+# Constants
+SERVER = 'chat.freenode.net'
+PORT = 6667
+REALNAME = NICK = "ateloph_posi"
+IDENT = "posiputt"
 
 '''
 Secure shutdown: 
@@ -8,54 +16,57 @@ Outcome: Quits program.
 '''
 def shutdown(socket, msg, buf):
     socket.close()
-    out = open('log', 'a')
-    out.write(buf+'\n')
+    with  open('log', 'a') as out:
+        out.write(buf+'\n')
     out.close()
     print msg
-    quit()
-
-# Constants
-SERVER = 'chat.freenode.net'
-PORT = 6667
-REALNAME = NICK = "ateloph_posi"
-IDENT = "posiputt"
+    sys.exit("Exiting. Log has been written.")
+    
 
 # connect to server
-s = socket.socket()
-s.connect((SERVER, PORT))
-s.send('NICK ' + NICK + '\n')
-s.send('USER ' + IDENT + ' ' + SERVER +' bla: ' + REALNAME + '\n')
-
-line = s.recv(500)
-print line
-# log first line into buffer
-buf = line
-
-while True:
-    line = s.recv(500)
-    buf += line # log all the lines!
+def conbot():
+    s = socket.socket()
+    s.connect((SERVER, PORT))
+    s.send('NICK ' + NICK + '\n')
+    s.send('USER ' + IDENT + ' ' + SERVER +' bla: ' + REALNAME + '\n')
+    return s
+    
+if __name__ == '__main__':
+    s = conbot()
+    
+    # Generate a List line and List "buffer" buf.
+    buf = line = s.recv(500)
     print line
 
-    #join AFTER connect is complete
-    if line.find('Welcome to the freenode') != -1:
-        s.send('JOIN #botophobia\n')
+    try:
+        while True:
+            line = s.recv(500)
+            buf += line # log all the lines!
+            print line
 
-    # rude quit command (from anyone)
-    if line.find('hau ab') != -1:
-        shutdown(s, "ich geh ja schon", buf)
+            #join AFTER connect is complete
+            if line.find('Welcome to the freenode') != -1:
+                s.send('JOIN #5\n')
 
-    # catch disconnect
-    if line.find(':Closing Link:') != -1:
-        shutdown(s, "connection lost", buf)
+            # rude quit command (from anyone)
+            if line.find('hau ab') != -1:
+                shutdown(s, "ich geh ja schon", buf)
 
-    line = line.rstrip()
-    line = line.split()
+            # catch disconnect
+            if line.find(':Closing Link:') != -1:
+                shutdown(s, "connection lost", buf)
 
-    # Test method:
-    # Bot should reply with 'pong' if 'ping'ed.
-    if (line[0] == 'PING'):
-        pong = 'PONG '+line[1]+'\n'
-        print pong
-        s.send(pong)
+            line = line.rstrip().split()
+            print line
 
-shutdown(s, "end of program", buf)
+            # Test method:
+            # Bot should reply with 'pong' if 'ping'ed.
+            if (line[0] == 'PING'):
+                pong = 'PONG '+line[1]+'\n'
+                print pong
+                s.send(pong)
+                
+    except Exception as e:
+        shutdown(s, e, buf)
+        raise e
+
