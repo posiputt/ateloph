c0117336497aca6817244ce0413808c227a629e9
posiputt <i@posiputt.de>
2015-01-16 15:58:37 +0100

    minibot.py works, bot.py removed

--- Sandbox/minibot.py
+++ Sandbox/minibot.py
@@ -2,16 +2,23 @@ import socket
 
 s = socket.socket()
 s.connect(('chat.freenode.net', 6667))
-s.send('NICK atelophn')
-s.send('USER posiputt irc.freenode.net bla: atelophbotn')
+line = s.recv(500)
+print "moop: " + line
+s.send('NICK ateloph\n')
+s.send('USER posiputt irc.freenode.net bla: atelophbot\n')
 
+i = 0
 while True:
+    print i,
+    i += 1
     line = s.recv(500)
     print line
     if line.find('Welcome to the freenode') != -1:
-        s.send('JOIN #5n')
+        s.send('JOIN #5\n')
     if line.find('PRIVMSG') != -1:
         line = line.rstrip()
         line = line.split()
         if (line[0] == 'PING'):
-            s.send('PONG '+line[1]+'n')
+            pong = 'PONG'+line[1]+'\n'
+            print pong
+            s.send(pong)
