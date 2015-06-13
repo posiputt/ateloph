0eef7c439ecdbf05142927f7d62753352d2de4ca
posiputt <i@posiputt.de>
2015-06-13 17:37:03 +0200

    added class methods run (main loop), listen, parse, and join

--- dev_ateloph.py
+++ dev_ateloph.py
@@ -26,33 +26,68 @@ class connection:
         self.REALNAME = realname
         self.NICKNAME = nickname
         self.IDENT = ident
+        self.EOL = '\n'
         
+    def run(self):
+        print "[>] Connecting to " + self.SERVER
+        try:
+            self.connect()
+        except Exception as e:
+            print "[!] Error: " + str(e)
+        stub = ''
+        while True:
+            stream = stub + self.listen(4096)
+            lines = stream.split(self.EOL)
+            if stream[-1] != self.EOL:
+                stub = lines.pop(-1)
+            else:
+                stub = ''
+            for l in lines:
+                print "[RAW] " + l
+                self.parse(l)
+                
     def connect(self):
         self.s = socket.socket()
         self.s.connect((self.SERVER, self.PORT))
-        self.s.send('NICK ' + self.NICKNAME + '\n')
-        self.s.send('USER ' + self.IDENT + ' ' + self.SERVER +' bla: ' + self.REALNAME + '\n')
+        self.s.send('NICK ' + self.NICKNAME + self.EOL)
+        self.s.send('USER ' + self.IDENT + ' ' + self.SERVER +' bla: ' + self.REALNAME + self.EOL)
         
-    def listen(self):
-        lines = []
-        stub = ''
-        '''
-        this obviously cannot stay
-        an infinite loop
-        '''
-        while True:
-            stream = self.s.recv(2048)
-            print stream,
-            time.sleep(1)
-        self.s.close()
+    def listen(self, chars):
+        s_ready = select.select([self.s],[],[],10)
+        if s_ready:
+            return self.s.recv(chars)
+    
+    def parse(self, line):
+        if line == '':
+            return
+        log_this = ['PRIVMSG', 'JOIN', 'PART', 'KICK', 'TOPIC']
+        words = line.split(' ')
+        if words[0] == 'PING':
+            pong = 'PONG ' + words[1] + self.EOL
+            self.s.send(pong)
+            print "[P] " + pong
+        elif words[0][0] == ':':
+            sender = words[0]
+            indicator = words[1]
+            if indicator in log_this:
+                channel = words[2]
+                message = ' '.join(words[3:])
+                print "[L] " + sender + ' ' + channel + ' ' + message
+            else:
+                if indicator == '376':
+                    self.join()
+    
+    def join(self):
+        print "[J] Joining " + self.CHANNEL
+        self.s.send('JOIN ' + self.CHANNEL + self.EOL)
+        
 
 if __name__ == '__main__':
     server = 'chat.freenode.net'
     port = 6667
-    channel = '#5'
+    channel = '#dumme-gesellschaft'
     realname = 'ateloph test'
     nickname = 'ateloph_test'
     ident = 'posiputt'
     freenode = connection(server, port, channel, realname, nickname, ident)
-    freenode.connect()
-    freenode.listen()
+    freenode.run()
