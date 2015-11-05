4e54b5c4e1bc5da6c0c7168e11bda1ea2d00878d
posiputt <i@posiputt.de>
2015-11-05 22:07:08 +0100

    minor changes

--- dev_ateloph.py
+++ dev_ateloph.py
@@ -132,13 +132,14 @@ class Connection:
                                     req = requests.get(w)
                                     tree = fromstring(req.content)
                                     title = tree.findtext('.//title')
-                                    post_to_chan = " ".join((title, w))
+                                    # post_to_chan = " ".join((title, w))
+                                    post_to_chan = " ".join(("Page title:", title))
                                     post_to_chan = post_to_chan.replace("\n", " ")
                                     # print(post_to_chan)
                                 except:
                                     post_to_chan = " ".join((nick+":", "Sorry, couldn't fetch page title."))
                                 what_the_bot_said = post_to_chan
-                                post_to_chan = "NOTICE " + channel + " :" + post_to_chan + self.EOL
+                                post_to_chan = "PRIVMSG " + channel + " :" + post_to_chan + self.EOL
                                 print(post_to_chan)
                                 self.s.send(post_to_chan.encode('utf-8'))
                             else:
